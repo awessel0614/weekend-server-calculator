e216e12998eaa842d8af5b010bd9875967680b71
Ashley Wessel <amerino.dance@gmail.com>
2023-10-07 14:36:57 -0500

    got the clear button to work

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -10,9 +10,10 @@ let operator ;
 //when the = button is pressed, it needs to capture the two number values and the value of the mathematical operator
 //the operator part is the one where im a little like, eh?
 
-
+getMathFromServer();
 
 function getMathFromServer() {
+    
     console.log('Start getMathFromServer');
     axios.get('/result').then((response) => {
         console.log(response.data);
@@ -22,12 +23,10 @@ function getMathFromServer() {
         let resultArea = document.querySelector('#result');
         let mathHistoryArea = document.querySelector('#math-history');
 
-
         resultArea.innerHTML ='';
         resultArea.innerHTML = `
             ${numbers.results}
         `;
-
 
         mathHistoryArea.innerHTML = '';
         let i = 0;
@@ -83,5 +82,40 @@ function handleOperator(event) {
     event.preventDefault();
     operator = event.target.innerHTML;
 
-
 }
+
+
+
+
+
+/** 
+function clearNumbersFromBoxes(event) {
+
+    console.log('Beginning clearNumbersFromBoxes');
+
+    let firstNumberText = document.querySelector('#first-number').value;
+    let secondNumberText = document.querySelector('#second-number').value;
+
+    console.log(firstNumberText);
+    console.log(secondNumberText);
+
+    firstNumberText.innerHTML.value = '';
+    secondNumberText.innerHTML.value = '';
+
+    console.log(firstNumberText);
+    console.log(secondNumberText);
+
+    console.log('End clearNumbersFromBoxes');
+}
+*/
+
+let clearButton = document.querySelector('#clear-button');
+let firstNumberInput = document.querySelectorAll('#first-number');
+let secondNumberInput = document.querySelectorAll('#second-number');
+clearButton.addEventListener('click', () => {
+    firstNumberInput.forEach(input => input.value = '');
+    secondNumberInput.forEach(input => input.value = '');
+});
+
+
+
